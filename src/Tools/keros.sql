ac85964570f79751f03a88e0dc7509aecd3e5f39
laurenttainturier <laurent.tainturier.pro@gmail.com>
2018-07-13 22:21:09 +0200

    deletion of local sql scripts

--- src/Tools/keros.sql
+++ src/Tools/keros.sql
@@ -4,9 +4,9 @@ SET UNIQUE_CHECKS = 0;
 
 DROP TABLE IF EXISTS cat;
 CREATE TABLE cat (
-  id     INT NOT NULL AUTO_INCREMENT,
-  name   VARCHAR(255),
-  height FLOAT(5, 2),
+  id     int NOT NULL AUTO_INCREMENT,
+  name   varchar(255),
+  height float(5, 2),
   PRIMARY KEY (id)
 )
   ENGINE = InnoDB
@@ -14,9 +14,9 @@ CREATE TABLE cat (
 
 DROP TABLE IF EXISTS core_pole;
 CREATE TABLE `core_pole` (
-  `id`    INT(5) AUTO_INCREMENT,
-  `label` VARCHAR(5)  NOT NULL UNIQUE,
-  `name`  VARCHAR(40) NOT NULL UNIQUE,
+  `id`    int(5) AUTO_INCREMENT,
+  `label` varchar(5)  NOT NULL UNIQUE,
+  `name`  varchar(40) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
@@ -24,9 +24,10 @@ CREATE TABLE `core_pole` (
 
 DROP TABLE IF EXISTS core_position;
 CREATE TABLE `core_position` (
-  `id`     INT(11) AUTO_INCREMENT,
-  `label`  VARCHAR(64) NOT NULL UNIQUE,
-  `poleId` INT(5),
+  `id`     int(11) AUTO_INCREMENT,
+  `label`  varchar(64) NOT NULL UNIQUE,
+  `poleId` int(5),
+  CONSTRAINT `core_position_poleId_fk` FOREIGN KEY (`poleId`) REFERENCES `core_pole` (`id`),
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
@@ -34,9 +35,9 @@ CREATE TABLE `core_position` (
 
 DROP TABLE IF EXISTS core_department;
 CREATE TABLE `core_department` (
-  `id`    INT(11) AUTO_INCREMENT,
-  `label` VARCHAR(15) NOT NULL UNIQUE,
-  `name`  VARCHAR(64) NOT NULL UNIQUE,
+  `id`    int(11) AUTO_INCREMENT,
+  `label` varchar(15) NOT NULL UNIQUE,
+  `name`  varchar(64) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
@@ -44,20 +45,18 @@ CREATE TABLE `core_department` (
 
 DROP TABLE IF EXISTS core_country;
 CREATE TABLE `core_country` (
-  `id`    INT(11) AUTO_INCREMENT,
-  `label` VARCHAR(64) NOT NULL UNIQUE,
+  `id`    int(11) AUTO_INCREMENT,
+  `label` varchar(64) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-
 DROP TABLE IF EXISTS core_user;
-
 CREATE TABLE `core_user` (
-  `id`              INT(11)               AUTO_INCREMENT,
-  `username`        VARCHAR(50)  NOT NULL UNIQUE,
-  `password`        VARCHAR(100) NOT NULL,
+  `id`              int(11)               AUTO_INCREMENT,
+  `username`        varchar(50)  NOT NULL UNIQUE,
+  `password`        varchar(100) NOT NULL,
   `lastConnectedAt` DATETIME,
   `createdAt`       DATETIME              DEFAULT NOW(),
   `disabled`        BOOLEAN      NOT NULL DEFAULT FALSE,
@@ -70,7 +69,7 @@ CREATE TABLE `core_user` (
 
 DROP TABLE IF EXISTS core_gender;
 CREATE TABLE `core_gender` (
-  `id`    INT(1) AUTO_INCREMENT,
+  `id`    int(1) AUTO_INCREMENT,
   `label` VARCHAR(1) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 )
@@ -79,48 +78,55 @@ CREATE TABLE `core_gender` (
 
 DROP TABLE IF EXISTS core_address;
 CREATE TABLE `core_address` (
-  `id`         INT(1) AUTO_INCREMENT,
+  `id`         int(1) AUTO_INCREMENT,
   `line1`      VARCHAR(64) NOT NULL,
   `line2`      VARCHAR(64),
   `postalCode` VARCHAR(10) NOT NULL,
   `city`       VARCHAR(64) NOT NULL,
-  `countryId`  INT(11)     NOT NULL,
-  PRIMARY KEY (`id`)
+  `countryId`  int(11)     NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `core_address_countryId_fk` FOREIGN KEY (`countryId`) REFERENCES `core_country` (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-# L ID de core_member est le même le core_user qui lui est attaché"
+# L'ID de core_member est le même le core_user qui lui est attaché
 DROP TABLE IF EXISTS core_member;
 CREATE TABLE `core_member` (
-  `id`           INT(11)      NOT NULL,
-  `genderId`     INT(1)       NOT NULL,
-  `firstName`    VARCHAR(100) NOT NULL,
-  `lastName`     VARCHAR(100) NOT NULL,
-  `birthdate`    DATE        DEFAULT NULL,
-  `telephone`    VARCHAR(20) DEFAULT NULL UNIQUE,
-  `email`        VARCHAR(255) NOT NULL UNIQUE,
-  `addressId`    INT(11)      NOT NULL UNIQUE,
-  `schoolYear`   INT(11)     DEFAULT NULL,
-  `departmentId` INT(11)     DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id`           int(11)      NOT NULL,
+  `genderId`     int(1)       NOT NULL,
+  `firstName`    varchar(100) NOT NULL,
+  `lastName`     varchar(100) NOT NULL,
+  `birthdate`    date        DEFAULT NULL,
+  `telephone`    varchar(20) DEFAULT NULL UNIQUE,
+  `email`        varchar(255) NOT NULL UNIQUE,
+  `addressId`    int(11)      NOT NULL UNIQUE,
+  `schoolYear`   int(11)     DEFAULT NULL,
+  `departmentId` int(11)     DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `core_user_userId_fk` FOREIGN KEY (`id`) REFERENCES `core_user` (`id`),
+  CONSTRAINT `core_user_genderId_fk` FOREIGN KEY (`genderId`) REFERENCES `core_gender` (`id`),
+  CONSTRAINT `core_user_addressId_fk` FOREIGN KEY (`addressId`) REFERENCES `core_address` (`id`),
+  CONSTRAINT `core_user_departmentId_fk` FOREIGN KEY (`departmentId`) REFERENCES `core_department` (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 DROP TABLE IF EXISTS core_member_position;
 CREATE TABLE `core_member_position` (
-  `memberId`   INT(11) NOT NULL,
-  `positionId` INT(11) NOT NULL,
-  PRIMARY KEY (`memberId`, `positionId`)
+  `memberId`   int(11) NOT NULL,
+  `positionId` int(11) NOT NULL,
+  PRIMARY KEY (`memberId`, `positionId`),
+  CONSTRAINT `core_member_position_memberId_fk` FOREIGN KEY (`memberId`) REFERENCES `core_member` (`id`),
+  CONSTRAINT `core_position_position_positionId_fk` FOREIGN KEY (`positionId`) REFERENCES `core_position` (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 DROP TABLE IF EXISTS ua_firm_type;
 CREATE TABLE `ua_firm_type` (
-  `id`    INT(2) AUTO_INCREMENT,
-  `label` VARCHAR(15) NOT NULL UNIQUE,
+  `id`    int(2) AUTO_INCREMENT,
+  `label` varchar(15) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
@@ -128,15 +134,18 @@ CREATE TABLE `ua_firm_type` (
 
 DROP TABLE IF EXISTS ua_firm;
 CREATE TABLE `ua_firm` (
-  `id`        INT(11) AUTO_INCREMENT,
-  `siret`     VARCHAR(20) UNIQUE,
-  `name`      VARCHAR(64) NOT NULL UNIQUE,
-  `addressId` INT(11)     NOT NULL UNIQUE,
-  `typeId`    INT(11)     NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
+  `id`        int(11) AUTO_INCREMENT,
+  `siret`     varchar(20) UNIQUE,
+  `name`      varchar(64) NOT NULL UNIQUE,
+  `addressId` int(11)     NOT NULL UNIQUE,
+  `typeId`    int(11)     NOT NULL UNIQUE,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `ua_firm_address_addressId_fk` FOREIGN KEY (`addressId`) REFERENCES `core_address` (`id`),
+  CONSTRAINT `ua_firm_type_typeId_fk` FOREIGN KEY (`typeId`) REFERENCES `ua_firm_type` (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
 
 SET AUTOCOMMIT = 1;
 SET FOREIGN_KEY_CHECKS = 1;
@@ -156,9 +165,9 @@ INSERT INTO `core_pole` (id, label, name) VALUES
   (10, 'UA', 'Unité d\'affaires');
 
 INSERT INTO `core_position` (id, label, poleId) VALUES
-  (1, 'Ancien membre', NULL), (2, 'Auditeur CNJE', NULL), (3, 'Chargé d''affaires', 10), (4, 'Client', NULL),
-  (5, 'Comptable', 9), (6, 'Consultant', NULL), (7, 'Junior Com', 1), (8, 'Junior DSI', 3), (9, 'Junior GRC', 4),
-  (10, 'Junior Qualité', 6), (11, 'Membre CNJE', NULL), (12, 'Président', 5), (13, 'Responsable BU', 10),
+  (1, 'Ancien membre', null), (2, 'Auditeur CNJE', null), (3, 'Chargé d''affaires', 10), (4, 'Client', null),
+  (5, 'Comptable', 9), (6, 'Consultant', null), (7, 'Junior Com', 1), (8, 'Junior DSI', 3), (9, 'Junior GRC', 4),
+  (10, 'Junior Qualité', 6), (11, 'Membre CNJE', null), (12, 'Président', 5), (13, 'Responsable BU', 10),
   (14, 'Responsable Com', 1), (15, 'Responsable DSI', 3), (16, 'Responsable GRC', 4), (17, 'Responsable Qualité', 6),
   (18, 'Responsable UA', 10), (19, 'Secrétaire Général', 8), (20, 'Trésorier', 9), (21, 'Vice-Président', 5),
   (22, 'Vice-Trésorier', 9);
--- src/Tools/kerosData.sql
+++ src/Tools/kerosData.sql
@@ -2,19 +2,19 @@ SET AUTOCOMMIT = 0;
 SET FOREIGN_KEY_CHECKS = 0;
 SET UNIQUE_CHECKS = 0;
 
-
+TRUNCATE TABLE cat;
 INSERT INTO cat (id, name, height) VALUES
   (1, 'Tom', 7.14),
   (2, 'Kevin', 7.14),
   (3, 'Patch', 7.14);
 
-
+TRUNCATE TABLE core_user;
 INSERT INTO core_user (id, username, password, expiresAt) VALUES
   (1, 'cbreeze', 'hunter11' , STR_TO_DATE('5/15/2022 8:06:26 AM', '%c/%e/%Y %r')),
   (2, 'mcool', 'hunter12' , STR_TO_DATE('5/15/2022 8:06:26 AM', '%c/%e/%Y %r')),
   (3, 'lswollo', 'hunter13' , STR_TO_DATE('5/15/2022 8:06:26 AM', '%c/%e/%Y %r'));
 
-
+TRUNCATE TABLE core_address;
 INSERT INTO core_address (id, line1, line2, postalCode, city, countryId) VALUES
   (1, '13 rue renard', null, '69100', 'lyon', 1), # member 1
   (2, '11 baker street', 'appt 501', '6930A', 'dublin', 2), # member 2
@@ -22,13 +22,13 @@ INSERT INTO core_address (id, line1, line2, postalCode, city, countryId) VALUES
   (4, '11 backbeat street', 'bat. a', '91004', 'djibouti', 3), # firm 1
   (5, '17 watcha ave', 'porte 5', '674A4', 'Leicester', 40); # firm 2
 
-
+TRUNCATE TABLE core_member;
 INSERT INTO core_member (id, genderId, firstName, lastName, birthdate, telephone, email, addressId, schoolYear, departmentId) VALUES
   (1, 1, 'Conor', 'Breeze', STR_TO_DATE('1975-12-25', '%Y-%m-%d'), '+332541254', 'fake.mail@fake.com', 2, 3, 1),
   (2, 1, 'Marah', 'Cool', STR_TO_DATE('1976-10-27', '%Y-%m-%d'), '+332541541', 'fake.mail2@fake.com', 1, 3, 1),
   (3, 1, 'Lolo', 'Swollo', STR_TO_DATE('1987-12-2', '%Y-%m-%d'), '+337425254', 'fake.mail3@fake.com', 3, 5, 2);
 
-
+TRUNCATE TABLE core_member_position;
 INSERT INTO core_member_position (memberId, positionId) VALUES
   (1, 3),
   (2, 3),
@@ -36,7 +36,7 @@ INSERT INTO core_member_position (memberId, positionId) VALUES
   (3, 2),
   (3, 3);
 
-
+TRUNCATE TABLE ua_firm;
 INSERT INTO ua_firm (id, siret, name, addressId, typeId) VALUES
   (1, '215437645', 'Cool Inc.', 4, 3),
   (2, '471245896', 'Swagger', 5, 1);
