6757f324cea75da2bd8b3faba9cb7fe2fefdfe4c
Conor Ryan <cr.in.france@gmail.com>
2018-07-18 12:53:21 +0200

    Update DepartmentIntegrationTest.php

--- tests/Core/DepartmentIntegrationTest.php
+++ tests/Core/DepartmentIntegrationTest.php
@@ -22,7 +22,7 @@ class DepartmentIntegrationTest extends AppTestCase
         $this->assertSame($response->getStatusCode(), 200);
 
         $body = json_decode($response->getBody());
-        $this->assertEquals(2, sizeof($body));
+        $this->assertEquals(13, sizeof($body));
     }
     public function testGetDepartmentShouldReturn200()
     {
@@ -38,14 +38,14 @@ class DepartmentIntegrationTest extends AppTestCase
 
         $body = json_decode($response->getBody());
         $this->assertSame($body->id, 1);
-        $this->assertSame($body->label, "IF");
-        $this->assertSame($body->name, "Informatique");
+        $this->assertSame($body->label, "BB");
+        $this->assertSame($body->name, "Biochimie et Biotechnologies");
     }
     public function testGetDepartmentShouldReturn400()
     {
         $env = Environment::mock([
             'REQUEST_METHOD' => 'GET',
-            'REQUEST_URI' => '/api/v1/core/department/3',
+            'REQUEST_URI' => '/api/v1/core/department/101',
         ]);
         $req = Request::createFromEnvironment($env);
         $this->app->getContainer()['request'] = $req;
