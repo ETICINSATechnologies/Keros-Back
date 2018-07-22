d1290de7b2eae9f495f4da9b45ab336404c94f30
laurenttainturier <laurent.tainturier.pro@gmail.com>
2018-07-22 17:01:12 +0200

    addition address creation tests

--- tests/Core/AddressTest.php
+++ tests/Core/AddressTest.php
@@ -3,27 +3,31 @@
 namespace KerosTest\Address;
 
 use Keros\Entities\core\Address;
+use Keros\Entities\Core\Country;
 use PHPUnit\Framework\TestCase;
 
-/*
+
 final class AddressTest extends TestCase
 {
     public function testNewAddressShouldBeInstanceOfAddress()
     {
-        # @TODO: modify this test
         $this->assertInstanceOf(Address::class,
-                                new Address("18 rue du master", "", 69100, "Lyon", 62));
+                                new Address("18 rue du master", "", 69100, "Lyon"));
     }
 
     public function testAddressShouldCreateWithParams()
     {
-        $address = new Address("18 rue du master", "", 69000, "Lyon", 62);
+        $address = new Address("18 rue du master", "", 69000, "Lyon");
+        $country = new Country("Eldorado");
+        $address->setCountry($country);
+
         $this->assertInstanceOf(Address::class, $address);
         $this->assertEquals("18 rue du master", $address->getLine1());
         $this->assertEquals("", $address->getLine2());
         $this->assertEquals(69000, $address->getPostalCode());
         $this->assertEquals("Lyon", $address->getCity());
-        $this->assertEquals(62, $address->getCountry());
+        $this->assertEquals("Eldorado", $address->getCountry()->id);
+        $this->assertNull($address->getCountry()->id);
         $this->assertNull($address->getId());
     }
-}*/
+}
