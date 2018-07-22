9459fd6b3ac78e02bfd01f973719195b01c570c6
laurenttainturier <laurent.tainturier.pro@gmail.com>
2018-07-22 16:13:56 +0200

    comment AddressTest class

--- tests/Core/AddressTest.php
+++ tests/Core/AddressTest.php
@@ -5,16 +5,17 @@ namespace KerosTest\Address;
 use Keros\Entities\core\Address;
 use PHPUnit\Framework\TestCase;
 
+/*
 final class AddressTest extends TestCase
 {
-    /*public function testNewAddressShouldBeInstanceOfAddress()
+    public function testNewAddressShouldBeInstanceOfAddress()
     {
         # @TODO: modify this test
         $this->assertInstanceOf(Address::class,
                                 new Address("18 rue du master", "", 69100, "Lyon", 62));
-    }*/
+    }
 
-    /*public function testAddressShouldCreateWithParams()
+    public function testAddressShouldCreateWithParams()
     {
         $address = new Address("18 rue du master", "", 69000, "Lyon", 62);
         $this->assertInstanceOf(Address::class, $address);
@@ -24,5 +25,5 @@ final class AddressTest extends TestCase
         $this->assertEquals("Lyon", $address->getCity());
         $this->assertEquals(62, $address->getCountry());
         $this->assertNull($address->getId());
-    }*/
-}
+    }
+}*/
