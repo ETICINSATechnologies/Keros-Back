e436f596c41d20823233f3435a8c040b95dc666f
Conor Ryan <conor.ryan@atos.net>
2018-08-20 14:12:10 +0200

    Renamed "dev" to "staging", changed incorrect imports, changed code to match API specs

--- .deploy/deploy-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sshpass -p "${SSH_PASS}" rsync -r --delete-after --quiet --exclude '/src/settings.ini' $TRAVIS_BUILD_DIR/src ${SSH_USER}@${SSH_HOST}:/var/www/keros-api-dev
-sshpass -p "${SSH_PASS}" ssh ${SSH_USER}@${SSH_HOST} "(cd /var/www/keros-api-dev; composer install )"
