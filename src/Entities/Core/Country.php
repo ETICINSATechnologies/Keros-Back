98e8b9215f2acfbc6d855a5d490100a6f56469a0
neeeekitos <nikita.a.terekhov@gmail.com>
2019-11-17 20:06:59 +0100

    le nouvel attribut isEu est ajouté à l'entité counrty

--- src/Entities/Core/Country.php
+++ src/Entities/Core/Country.php
@@ -25,13 +25,18 @@ class Country implements JsonSerializable, Searchable
     /** @Column(type="string", length=64) */
     protected $label;
 
+    /** @Column(type="boolean") */
+    protected $isEu;
+
     /**
      * Country constructor.
      * @param $label
+     * @param $isEu
      */
-    public function __construct($label)
+    public function __construct($label, $isEu)
     {
         $this->label = $label;
+        $this->isEu = $isEu;
     }
 
     public function jsonSerialize()
@@ -39,6 +44,7 @@ class Country implements JsonSerializable, Searchable
         return [
             'id' => $this->getId(),
             'label' => $this->getLabel(),
+            'isEu' => $this->getIsEu()
         ];
     }
 
@@ -69,5 +75,13 @@ class Country implements JsonSerializable, Searchable
     public function setLabel($label): void
     {
         $this->label = $label;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsEu()
+    {
+        return $this->isEu;
     }
 }
--- tests/Core/AddressTest.php
+++ tests/Core/AddressTest.php
@@ -12,12 +12,12 @@ class AddressTest extends TestCase
     public function testNewAddressShouldBeInstanceOfAddress()
     {
         $this->assertInstanceOf(Address::class,
-                                new Address("18 rue du master", "", 69100, "Lyon", new Country("Eldorado")));
+                                new Address("18 rue du master", "", 69100, "Lyon", new Country("Eldorado", false)));
     }
 
     public function testAddressShouldCreateWithParams()
     {
-        $address = new Address("18 rue du master", "", 69000, "Lyon", new Country("Eldorado"));
+        $address = new Address("18 rue du master", "", 69000, "Lyon", new Country("Eldorado", false));
 
         $this->assertInstanceOf(Address::class, $address);
         $this->assertEquals("18 rue du master", $address->getLine1());
@@ -25,6 +25,7 @@ class AddressTest extends TestCase
         $this->assertEquals(69000, $address->getPostalCode());
         $this->assertEquals("Lyon", $address->getCity());
         $this->assertEquals("Eldorado", $address->getCountry()->getLabel());
+        $this->assertEquals(false, $address->getCountry()->getIsEu());
         $this->assertNull($address->getCountry()->getId());
         $this->assertNull($address->getId());
     }
