4471e85552e4f0c4a384a61d0e19c716af6a49e2
Sadsitha Lokuge <slokuge97@gmail.com>
2020-06-16 23:55:01 +0200

    Fixing directories

--- .deploy/mysql.provision.sh
+++ .deploy/mysql.provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 mysql -u root -proot -e "DROP DATABASE IF EXISTS keros;"
-mysql -u root -proot -e "CREATE DATABASE keros;"
+mysql -u root -proot -e "CREATE DATABASE keros CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -u root -proot keros < /opt/Database/keros.sql
 mysql -u root -proot keros < /opt/Database/kerosData.sql
